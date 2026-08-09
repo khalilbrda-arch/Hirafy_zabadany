deea450f6b967d4cdbe01ff649d8e4d45f5623b3
khalilbrda-arch <khalil.brda@gmail.com>
2026-08-09 16:13:19 +0300

    Create Profile component with account information

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,10 @@
+const Profile = () => {
+  return (
+    <div className="p-6 text-center">
+      <h2 className="text-2xl font-medium">حسابي</h2>
+      <p className="text-dark-text/70 mt-2">معلومات الحساب</p>
+    </div>
+  )
+}
+
+export default Profile
