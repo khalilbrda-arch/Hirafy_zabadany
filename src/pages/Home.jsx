ce2e57bbf5374b70aa26a921ef25bd771ac4aedb
khalilbrda-arch <khalil.brda@gmail.com>
2026-08-09 16:10:20 +0300

    Add Home component with welcome message

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div className="p-6 text-center">
+      <h2 className="text-2xl font-medium">مرحباً بك في حرفي الزبداني</h2>
+      <p className="text-dark-text/70 mt-2">الصفحة الرئيسية قيد التطوير</p>
+    </div>
+  )
+}
+
+export default Home
