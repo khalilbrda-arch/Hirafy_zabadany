2c03a6faf6880cd1693c690c1e697d3c32bd1dfa
khalilbrda-arch <khalil.brda@gmail.com>
2026-08-09 16:11:55 +0300

    Create MyOrders component for order display

---
+++ src/pages/MyOrders.jsx
@@ -0,0 +1,10 @@
+const MyOrders = () => {
+  return (
+    <div className="p-6 text-center">
+      <h2 className="text-2xl font-medium">طلباتي</h2>
+      <p className="text-dark-text/70 mt-2">لا توجد طلبات حالياً</p>
+    </div>
+  )
+}
+
+export default MyOrders
