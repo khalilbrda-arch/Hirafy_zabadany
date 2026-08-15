86e3b7180fb5594db2e78b06650d356f67da5abe
khalilbrda-arch <khalil.brda@gmail.com>
2026-08-15 13:20:06 +0300

    Add image compression before uploading

--- src/uploadImage.js
+++ src/uploadImage.js
@@ -1,6 +1,20 @@
+import imageCompression from 'browser-image-compression'
+
 export async function uploadImage(file) {
+  let fileToUpload = file
+
+  try {
+    fileToUpload = await imageCompression(file, {
+      maxSizeMB: 0.5,
+      maxWidthOrHeight: 1280,
+      useWebWorker: true,
+    })
+  } catch (err) {
+    console.error('فشل ضغط الصورة، سيتم رفعها بحجمها الأصلي', err)
+  }
+
   const formData = new FormData()
-  formData.append('file', file)
+  formData.append('file', fileToUpload)
   formData.append('upload_preset', 'harafi_unsigned')
 
   const response = await fetch(
